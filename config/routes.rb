7974df71bdaa76731958c39be0bfe6bffcca668d
sahanah-ganesh <sahanah.ganesh@gmail.com>
2019-05-15 22:35:34 +0000

    fix merge issue

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,14 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :destroy]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  # equal to --> resources :users, only: [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
